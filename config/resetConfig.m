55a9c22009d4b9c9246d38c043b46d0b50a95090
augustijn <av407@exeter.ac.uk>
2025-04-06 23:24:59 +0200

    added viewer. Need to merge all tables into one and dynamically choose columns to display in UItables next, allowing for image viewer selector

--- config/resetConfig.m
+++ config/resetConfig.m
@@ -4,8 +4,8 @@ config.name = "default_config";
 config.metric = struct;
 config.optimiser = struct;
 config.data = struct;
-
-config.data.ExtraColumns = {"IDName = getIDName(Name, Date)", "Date = getDate(Name)", "tester2 = getIDName(Name, Date)"};
+%"IDName = getIDName(Name, Date)", "Date = getDate(Name)"
+config.data.ExtraColumns = {};
 config.data.FetchDataCols = {"Image = getImg(Name, Path)", "Mask = getMask(Name, Path)"};
 
 callingFileDir = fileparts(mfilename('fullpath'));
